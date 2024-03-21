b5e3c27a3a797959a7c39945c5d28fb7f45058c2
Shlok <https://github.com/Silentfirst>
2024-03-22 02:43:35 +0530

    Added sqlite database and created user

--- controller/users.js
+++ controller/users.js
@@ -1,9 +1,34 @@
 const express= require('express')
 const path=require('path')
 const userRouter = express.Router()
+const sqlite3= require('sqlite3')
+const db = require('../database.js')
 
-const reqLog =(req)=> console.log(`Received request at ${req.url}`)
+const reqLog =(req,isPost)=>isPost==='post'? console.log(`Received post request at ${req.url}`) :console.log(`Received get request at ${req.url}`)
 
+userRouter.get('/getUsers', (req,res)=>{
+    reqLog(req)
+
+    // Query to select all rows from the users table
+    const query = 'SELECT * FROM users';
+
+    // Execute the query
+    db.all(query, [], (err, rows) => {
+    if (err) {
+        console.error('Error retrieving data:', err.message);
+        return;
+    }
+    
+    // Log the retrieved rows
+    console.log('Retrieved rows from users table:');
+    rows.forEach((row) => {
+        console.log(row);
+    });
+    });
+
+    res.status(200).send("Check console")
+
+})
 
 userRouter.get('/login',(req,res)=>{
     reqLog(req)
@@ -16,11 +41,41 @@ userRouter.get('/signup',(req,res)=>{
     res.status(200).sendFile( path.join(__dirname, '../frontend/signup.html')  )
 })
 
-userRouter.post('/signup',(req,res)=>{
-    reqLog(req)
+userRouter.post('/signup', (req,res)=>{
+    reqLog(req,"post")
     console.log(req.body)
-    res.status(200).send("Successfully Logged in")
-})
+    const {username,email,password}= req.body 
+    if(!username || !email || !password) { //did not receive full data
+        res.status(401).send("Error getting signup data, please try again")
+    }
+    //hash password
+    const hashedPassword=  password
+    try {
+
+        const query = `
+        INSERT INTO users (username, email, password)
+        VALUES (?, ?, ?)
+        `;
+
+        const params = [username, email, hashedPassword];
+
+        db.run(query, params, function(err) {
+            if (err) {
+                console.error(err)
+                return res.status(500).json({ message: 'Failed to create user.' });
+            }
+            // User created successfully
+            res.status(201).json({ message: 'User created successfully.' });
+        });
+
+    } catch (error) {
+        console.error('Error creating user:', error);
+        res.status(500).json({ message: 'Internal server error.' });
+    }
+
+
+
+ })
 
 
 module.exports= userRouter
