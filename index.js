615f09d599244860d0c864f1de2f1e8f649ad04f
Shlok <https://github.com/Silentfirst>
2024-03-26 09:09:53 +0530

    Added Cookies

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const dotenv = require(  'dotenv') 
-const userRouter = require('./controller/users')
-const feedRouter = require('./controller/feedRouter')
 const sqlite3= require('sqlite3')
 const db = require('./database.js')
-
+const cookieParser = require('cookie-parser')
+const userRouter = require('./controller/users')
+const feedRouter = require('./controller/feedRouter')
+const testRouter = require('./controller/testRouter.js')
 
 dotenv.config()
 const app = express()
@@ -12,32 +13,19 @@ const port = process.env.PORT || 5000
 
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cookieParser())
 
 
 //Controllers
 app.use('/v1/users',userRouter)
 app.use('/v1/feed',feedRouter)
-
+app.use('/test', testRouter)
 
 
 //Cleanup code
 // Close the database connection when the Node.js process exits
 const events= ['exit', 'SIGINT', 'SIGUSR1', 'SIGUSR2', 'uncaughtException', 'SIGTERM'];
 
-// events.forEach((eventType) => {
-//     process.on(eventType, () => {
-//         db.close((err) => {
-//             if (err) {
-//                 return console.error('Error closing database:', err.message);
-//             }
-//             console.log('Disconnected from the SQLite database.');
-//         });
-//     }); 
-// });
- 
-
-// Why this fail 
 ['exit', 'SIGINT', 'SIGUSR1', 'SIGUSR2', 'uncaughtException', 'SIGTERM'].forEach((eventType) => {
     process.on(eventType, () => {
         db.close((err) => {
